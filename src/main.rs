a004f7adc959a89f7ead885cfbc6768d5115ee34
zjp <jiping_zhou@foxmail.com>
2024-11-03 10:06:25 +0800

    define submodule_add

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use plugin_cargo::{logger, prelude::*};
 
+#[macro_use]
+extern crate tracing;
+
 mod repo;
+mod submodule_add;
+
+const REPOS: &str = "repos";
 
 fn main() -> Result<()> {
     logger::init();
---
+++ src/submodule_add.rs
@@ -0,0 +1,24 @@
+use crate::REPOS;
+use indexmap::IndexSet;
+use plugin_cargo::prelude::*;
+
+fn git_link(user: &str, repo: &str) -> String {
+    format!("https://github.com/{user}/{repo}.git")
+}
+
+fn submodule_add(user_repo: &str, set: &mut IndexSet<String>) -> Result<()> {
+    let _span = error_span!("submodule_add", user_repo).entered();
+
+    let split: Vec<_> = user_repo.split("/").collect();
+    let (user, repo) = (&split[0], &split[1]);
+    let link = git_link(user, repo);
+
+    if set.contains(&link) {
+        return Ok(());
+    }
+
+    let path = Utf8PathBuf::from_iter([REPOS, user, repo]);
+    duct::cmd!("git", "submodule", "add", link, path).run()?;
+
+    Ok(())
+}
