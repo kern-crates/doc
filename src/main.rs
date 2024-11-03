38d148bb0063448d08b90037406dd575a029f76b
zjp <jiping_zhou@foxmail.com>
2024-11-03 09:17:24 +0800

    repo: define SelfRepo and parse submodules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use plugin_cargo::{logger, prelude::*};
 
+mod repo;
+
 fn main() -> Result<()> {
     logger::init();
 
---
+++ src/repo.rs
@@ -0,0 +1,40 @@
+use eyre::ContextCompat;
+use git2::Repository;
+use plugin_cargo::{prelude::*, repo};
+
+pub struct SelfRepo {
+    this: Repository,
+    submodules: Vec<Submodule>,
+}
+
+pub struct Submodule {
+    local: Utf8PathBuf,
+    url: String,
+    repo: repo::Repo,
+}
+
+impl Submodule {
+    pub fn new(m: git2::Submodule) -> Result<Self> {
+        let local = Utf8PathBuf::from_path_buf(m.path().into()).unwrap();
+        let url = m.url().unwrap().to_owned();
+
+        let user_repo = url
+            .strip_prefix("https://github.com/")
+            .with_context(|| format!("{url} can't strip prefix `https://github.com/`"))?;
+        let user_repo = user_repo.strip_suffix(".git").unwrap_or(user_repo);
+        let repo = repo::Repo::new(user_repo, repo::RepoSource::Local(local.clone()))?;
+        Ok(Self { local, url, repo })
+    }
+}
+
+fn self_repo() -> Result<SelfRepo> {
+    let this = Repository::open(".")?;
+
+    let submodules = this
+        .submodules()?
+        .into_iter()
+        .map(Submodule::new)
+        .collect::<Result<_>>()?;
+
+    Ok(SelfRepo { this, submodules })
+}
