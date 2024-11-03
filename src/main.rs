e489ce0e73a1b7db4bd28e2e80dd6aedb219496d
zjp <jiping_zhou@foxmail.com>
2024-11-03 12:53:05 +0800

    main: read list.json as first argument and process it

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,15 @@ const REPOS: &str = "repos";
 fn main() -> Result<()> {
     logger::init();
 
+    let arg = std::env::args().nth(1);
+    let list_json = Utf8PathBuf::from(arg.as_deref().unwrap_or("list.json"));
+
+    let list: Vec<String> = serde_json::from_slice(&std::fs::read(&list_json)?)?;
+
+    let mut manage = manage::Manage::new()?;
+    for user_repo in &list {
+        manage.process(user_repo)?;
+    }
+
     Ok(())
 }
